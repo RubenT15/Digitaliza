505d7e1185b08b1b9f70b2768684d471358721c1
RubenT15 <47105903+RubenT15@users.noreply.github.com>
2019-01-28 13:15:45 +0100

    Create 06_for.py

---
+++ 06_for.py
@@ -0,0 +1,7 @@
+nombres=["Juan","Pepe","Fernando","Maricarmen","Carla","Rodrigo"]
+selected=[]
+for nombre in nombres:
+    print(nombre)
+    if "a" in nombre:
+        selected.append(nombre)
+print(selected)
